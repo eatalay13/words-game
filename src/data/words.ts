d197d1aa8d6cf8d1f9d498564073b2bd76c8ca8e
Emrah Atalay <emrahatalay92@gmail.com>
2024-08-06 11:01:16 +0300

    feat: Update page title and add word data

---
+++ src/data/words.ts
@@ -0,0 +1,106 @@
+import { Word } from "../types";
+
+const words: Word[] = [
+  {
+    word: "Emrah",
+    hint: "Bir isim",
+    level: "easy",
+  },
+  {
+    word: "Kodlama",
+    hint: "Bir eylem",
+    level: "medium",
+  },
+  {
+    word: "JavaScript",
+    hint: "Bir programlama dili",
+    level: "hard",
+  },
+  {
+    word: "React",
+    hint: "Bir kütüphane",
+    level: "medium",
+  },
+  {
+    word: "TypeScript",
+    hint: "Bir programlama dili",
+    level: "hard",
+  },
+  {
+    word: "Node.js",
+    hint: "Bir teknoloji",
+    level: "medium",
+  },
+  {
+    word: "HTML",
+    hint: "Bir işaretleme dili",
+    level: "easy",
+  },
+  {
+    word: "CSS",
+    hint: "Bir stil dili",
+    level: "easy",
+  },
+  {
+    word: "Python",
+    hint: "Bir programlama dili",
+    level: "medium",
+  },
+  {
+    word: "Java",
+    hint: "Bir programlama dili",
+    level: "medium",
+  },
+  {
+    word: "C#",
+    hint: "Bir programlama dili",
+    level: "medium",
+  },
+  {
+    word: "C++",
+    hint: "Bir programlama dili",
+    level: "medium",
+  },
+  {
+    word: "Go",
+    hint: "Bir programlama dili",
+    level: "medium",
+  },
+  {
+    word: "Ruby",
+    hint: "Bir programlama dili",
+    level: "medium",
+  },
+  {
+    word: "PHP",
+    hint: "Bir programlama dili",
+    level: "medium",
+  },
+  {
+    word: "Swift",
+    hint: "Bir programlama dili",
+    level: "medium",
+  },
+  {
+    word: "Kotlin",
+    hint: "Bir programlama dili",
+    level: "medium",
+  },
+  {
+    word: "Rust",
+    hint: "Bir programlama dili",
+    level: "medium",
+  },
+  {
+    word: "Dart",
+    hint: "Bir programlama dili",
+    level: "medium",
+  },
+  {
+    word: "Scala",
+    hint: "Bir programlama dili",
+    level: "medium",
+  },
+];
+
+export default words;
--- src/types/index.tsx
+++ src/types/index.tsx
@@ -4,7 +4,18 @@ export interface GameManager {
   isGameRunning: boolean;
   score: number;
   health: number;
+  gameStatus: GameState;
   resetScore: () => void;
   incrementScore: () => void;
   decrementScore: () => void;
+}
+
+export type GameState = "start" | "playing" | "end";
+
+export type WordLevel = "easy" | "medium" | "hard";
+
+export interface Word {
+  word: string;
+  hint: string;
+  level: WordLevel;
 }
