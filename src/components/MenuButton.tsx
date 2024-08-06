e984aa8484ee1e3fede3077e5e500c6120dc6247
Emrah Atalay <emrahatalay92@gmail.com>
2024-08-06 11:44:48 +0300

    chore: Update npm dependencies and add MenuButton component

---
+++ src/components/MenuButton.tsx
@@ -0,0 +1,30 @@
+import clsx from "clsx";
+import { ReactNode } from "react";
+
+interface MenuButtonProps {
+  variant: "play" | "continue" | "settings" | "exit";
+  children: ReactNode;
+  onClick?: () => void;
+}
+
+function MenuButton(props: MenuButtonProps) {
+  return (
+    <button
+      onClick={props.onClick}
+      className={clsx(
+        "px-4 py-2 rounded mt-4 font-bold",
+        props.variant === "settings" &&
+          "bg-blue-500 text-white hover:bg-blue-700",
+        props.variant === "play" &&
+          "bg-green-500 text-white hover:bg-green-700",
+        props.variant === "exit" && "bg-red-500 text-white hover:bg-red-700",
+        props.variant === "continue" &&
+          "bg-yellow-500 text-white hover:bg-yellow-600"
+      )}
+    >
+      {props.children}
+    </button>
+  );
+}
+
+export default MenuButton;
--- src/screens/StartScreen.tsx
+++ src/screens/StartScreen.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import MenuButton from "../components/MenuButton";
 
 interface StartScreenProps {
   startGame: () => void;
@@ -6,17 +7,29 @@ interface StartScreenProps {
 
 const StartScreen: React.FC<StartScreenProps> = ({ startGame }) => {
   return (
-    <div className="text-center">
-      <h1 className="text-4xl font-bold mb-4">Kelime Tahmin Oyunu</h1>
-      <p className="text-lg mb-6">
-        Rastgele seçilen bir kelimeyi tahmin etmeye çalışın!
-      </p>
-      <button
-        onClick={startGame}
-        className="bg-blue-500 text-white px-4 py-2 rounded"
-      >
-        Oyuna Başla
-      </button>
+    <div className="text-center bg-[url('/start_bg.jpg')] bg-cover h-screen w-full">
+      <div className="backdrop-blur-md h-full w-full flex items-center justify-center">
+        <div>
+          <h2 className="text-md lg:text-3xl font-bold mb-4 text-white bg-green-800 p-3 rounded-xl shadow-md shadow-green-600">
+            Kelime Tahmin Oyunu
+          </h2>
+          <div className="flex flex-col justify-center">
+            <MenuButton variant="continue">Devam Et</MenuButton>
+            <MenuButton onClick={startGame} variant="play">
+              Yeni Oyun
+            </MenuButton>
+            <MenuButton variant="settings">Ayarlar</MenuButton>
+            <MenuButton
+              variant="exit"
+              onClick={() => {
+                window.location.reload();
+              }}
+            >
+              Çıkış
+            </MenuButton>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
