788a2fdf09aa1c9ed1abf1ee9c60334addb96262
Emrah Atalay <emrahatalay92@gmail.com>
2024-08-08 18:28:14 +0300

    feat: Update theme color and badge component
    Update the theme color of the application to #FFC62F for a more vibrant look. Also, enhance the Badge component by adding a star icon and improving its styling.

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -7,7 +7,7 @@ interface AppBarProps {
 function AppBar({ children }: AppBarProps) {
   return (
     <header className="w-full flex justify-between px-4 mt-4">
-      <div className="w-9 h-9 hover:cursor-pointer bg-gamePink rounded-full flex items-center justify-center text-white font-bold text-xl border-2 border-white">
+      <div className="w-12 h-12 hover:cursor-pointer bg-gamePink rounded-full flex items-center justify-center text-white font-bold text-xl border-2 border-white">
         ?
       </div>
       <div className="flex space-x-2">{children}</div>
--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,8 +1,14 @@
+import { Star } from "lucide-react";
+
 const Badge = ({ number = 30 }) => {
   return (
-    <div className="bg-white rounded-full py-1 px-3 flex items-center">
-      <div className="w-4 h-4 bg-yellow-400 rounded-full mr-1"></div>
-      <span className="text-yellow-800 font-bold">{number}</span>
+    <div className="flex justify-start">
+      <div className="w-12 h-12 hover:cursor-pointer bg-gamePink rounded-full flex items-center justify-center z-10 text-white font-bold text-xl border-2 border-white">
+        <Star size={20} />
+      </div>
+      <div className="bg-white rounded-r-full h-8 mt-2 py-3 px-5 -ml-1.5 z-0 flex items-center">
+        <span className="text-yellow-800 font-bold">{number}</span>
+      </div>
     </div>
   );
 };
