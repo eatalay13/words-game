671b8f51fcb23b9491c9df5ba302bfa6fc69ff66
Emrah Atalay <emrahatalay92@gmail.com>
2024-08-08 18:54:37 +0300

    Update MenuButton component styling and props

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -12,8 +12,8 @@ function MenuButton(props: MenuButtonProps) {
     <button
       onClick={props.onClick}
       className={clsx(
-        "px-4 py-2 rounded-full border-b-4 mt-4 flex flex-row items-center justify-center text-2xl md:text-4xl font-luckiestGuy ring ring-white border-b-gray-600/40",
-        props.variant === "save" && "bg-gameCyan text-white",
+        "py-2 rounded-full border-b-4 mt-4 flex flex-row items-center justify-center text-2xl md:text-4xl font-luckiestGuy ring ring-white border-b-gray-600/40",
+        props.variant === "save" && "bg-gameCyan text-white px-4",
         props.variant === "play" && "bg-gameGreen text-white px-16"
       )}
     >
